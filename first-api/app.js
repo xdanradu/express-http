6cdfde68324346c778262b854b60f653ce9b8630
Dan Radu <dan.radu@msg.group>
2023-03-25 11:15:05 +0200

    1

--- first-api/app.js
+++ first-api/app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/user', (req, res) => {
+    res.send('user from DB')
 })
 
 app.listen(port, () => {
